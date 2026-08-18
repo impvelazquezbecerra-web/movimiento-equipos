107c5024088a9dc8e00bf7c9f6ecdfd3d778c26a
impvelazquezbecerra-web <imp.velazquezbecerra@gmail.com>
2026-08-18 12:28:48 -0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+ 
+st.set_page_config(
+  page_title="Movimiento de Equipos",
+  layout="wide"
+)
+
+st.title(
+  "Movimiento de Equipos"
+)
+
+archivo = st.file_uploader(
+  "Seleccione archivo Excel",
+  type=["xlsx"]
+)
+ 
+if archivo:
+  st.success(
+    "Archivo cargado correctamente"
+)
